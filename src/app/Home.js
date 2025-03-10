8864592a025ddc09ba489badd88e89433087f99a
AndreaDaFi <adavila9@cougarnet.uh.edu>
2025-03-10 12:12:31 -0500

    added logo

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Typography, Box } from '@mui/material';
+import Cougar from '../components/Cougar.png';
 
 const Home = () => {
   return (
@@ -7,8 +8,38 @@ const Home = () => {
       {/* Header Section */}
       <Box my={4}>
         <Typography variant="h1" gutterBottom align="center">
-          Welcome to CougarPost
+          Welcome to...
         </Typography>
+
+        {/*Cougar Post logo*/}
+        <figure style={{ textAlign: 'center' }}>
+            <img 
+              src={Cougar} 
+              alt="Logo" 
+              className="hover-image"  // Add the hover-image class here
+              style={{ 
+              maxWidth: '600px', 
+              height: 'auto', 
+              display: 'block', 
+              margin: '0 auto',
+              transition: 'transform 0.3s ease-in-out'  // Move transition into inline styles
+          }} />
+          <figcaption style={{ fontSize: '12px', color: '#555', marginTop: '8px' }}>
+            Logo reference image: 
+          <a 
+            href="https://www.dreamstime.com/cougar-growling-close-up-mountain-lion-image220929865" 
+            style={{ color: '#D32F2F' }} 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            Source Website
+          </a>
+          </figcaption>
+        </figure>
+
+        <style jsx>{`.hover-image:hover {transform: translateY(-25px);`}</style>
+
+        <Box my={4}></Box>
         <Typography variant="h6" color="textSecondary" align="center">
           We provide the best postal services for all your needs
         </Typography>
