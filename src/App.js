f91f48fe61ae34e8c09be665a71838d655fdffa9
AndreaDaFi <adavila9@cougarnet.uh.edu>
2025-03-11 15:18:52 -0500

    organized tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Container, Typography } from '@mui/material';
+import { AppBar, Toolbar, Button, Container, Menu, MenuItem } from '@mui/material';
 import CustLogin from './app/CustLogin';
 import EmpLogin from './app/EmpLogin';
 import Store from './app/Store';
@@ -21,60 +21,116 @@ import PackageDetails from './app/PackageDetails';
 import CustSignup from './app/CustSignup';
 import ClerkAddPackage from './app/ClerkAddPackage';
 import CustAddPackage from './app/CustAddPackage';
-import Checkout from './app/Checkout';//checkout if they order items from the store
-import PackageCheckOut from './app/PackageCheckOut';//checkout if they want to ship a package
+import Checkout from './app/Checkout'; // checkout if they order items from the store
+import PackageCheckOut from './app/PackageCheckOut'; // checkout if they want to ship a package
 import AddMyHours from './app/AddMyHours';
 
 const App = () => {
+  // State for dropdown menus
+  const [anchorElCustomer, setAnchorElCustomer] = useState(null);
+  const [anchorElAdmin, setAnchorElAdmin] = useState(null);
+  const [anchorElManager, setAnchorElManager] = useState(null);
+  const [anchorElClerk, setAnchorElClerk] = useState(null);
+  const [anchorElDriver, setAnchorElDriver] = useState(null);
+
+  const handleMenuClick = (event, setAnchorEl) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = (setAnchorEl) => {
+    setAnchorEl(null);
+  };
+
   return (
     <Router>
       <AppBar position="static" sx={{ color: '#ffffff', backgroundColor: '#D32F2F' }}>
         <Toolbar>
-          {/*pages anyone could see when openning the app*/}
+          {/*pages anyone could see when opening the app*/}
           <Button color="inherit" component={Link} to="/">Home</Button>
           <Button color="inherit" component={Link} to="/cust-login">Customer Login</Button>
           <Button color="inherit" component={Link} to="/emp-login">Employee Login</Button>
 
-          {/*pages customers should be able to see*/}
-          <Button color="inherit" component={Link} to="/store">store</Button>
-          <Button color="inherit" component={Link} to="/MyPackages">My Packages</Button>
-          <Button color="inherit" component={Link} to="/CustAddPackage">Ship a new package</Button>
+          {/* Customer Pages Dropdown */}
+          <Button color="inherit" onClick={(e) => handleMenuClick(e, setAnchorElCustomer)}>
+            Customer Pages
+          </Button>
+          <Menu
+            anchorEl={anchorElCustomer}
+            open={Boolean(anchorElCustomer)}
+            onClose={() => handleMenuClose(setAnchorElCustomer)}
+          >
+            <MenuItem component={Link} to="/store">Store</MenuItem>
+            <MenuItem component={Link} to="/MyPackages">My Packages</MenuItem>
+            <MenuItem component={Link} to="/CustAddPackage">Ship a New Package</MenuItem>
+          </Menu>
 
-          {/*EMPLOYEE PAGES*/}
-          <Button color="inherit" component={Link} to="/AddMyHours">Input the hours I worked</Button>
-          {/*pages Admin should be able to see*/}
-          <Button color="inherit" component={Link} to="/AddPO">Add a Post Office</Button>
-          <Button color="inherit" component={Link} to="/ViewPO">View Post Offices</Button>
-          <Button color="inherit" component={Link} to="/AddMngr">Add Manager</Button>
-          <Button color="inherit" component={Link} to="/ViewStaff">View Staff</Button>
+          {/* Admin Pages Dropdown */}
+          <Button color="inherit" onClick={(e) => handleMenuClick(e, setAnchorElAdmin)}>
+            Admin Pages
+          </Button>
+          <Menu
+            anchorEl={anchorElAdmin}
+            open={Boolean(anchorElAdmin)}
+            onClose={() => handleMenuClose(setAnchorElAdmin)}
+          >
+            <MenuItem component={Link} to="/AddPO">Add a Post Office</MenuItem>
+            <MenuItem component={Link} to="/ViewPO">View Post Offices</MenuItem>
+            <MenuItem component={Link} to="/AddMngr">Add Manager</MenuItem>
+            <MenuItem component={Link} to="/ViewStaff">View Staff</MenuItem>
+          </Menu>
 
-          {/*pages Managers should be able to see*/}
-          <Button color="inherit" component={Link} to="/AddStore">Add Items to the store</Button>
-          <Button color="inherit" component={Link} to="/AddStaff">Add a new Staff member</Button>
-          <Button color="inherit" component={Link} to="/MngrViewStaff">View my staff</Button>
-          <Button color="inherit" component={Link} to="/ViewStaffActivity">View my staff activity</Button>
-          
-          {/*pages Clerks should be able to see*/}
-          <Button color="inherit" component={Link} to="/ReStock">Update Stock</Button>
-          <Button color="inherit" component={Link} to="/ClerkAddPackage">Manual entry for new customer package</Button>
-          
-          {/*pages Drivers should be able to see*/}
-          <Button color="inherit" component={Link} to="/PackageStatus">Update a package status</Button>
-          
+          {/* Manager Pages Dropdown */}
+          <Button color="inherit" onClick={(e) => handleMenuClick(e, setAnchorElManager)}>
+            Manager Pages
+          </Button>
+          <Menu
+            anchorEl={anchorElManager}
+            open={Boolean(anchorElManager)}
+            onClose={() => handleMenuClose(setAnchorElManager)}
+          >
+            <MenuItem component={Link} to="/AddStore">Add Items to the Store</MenuItem>
+            <MenuItem component={Link} to="/AddStaff">Add a New Staff Member</MenuItem>
+            <MenuItem component={Link} to="/MngrViewStaff">View My Staff</MenuItem>
+            <MenuItem component={Link} to="/ViewStaffActivity">View My Staff Activity</MenuItem>
+          </Menu>
+
+          {/* Clerk Pages Dropdown */}
+          <Button color="inherit" onClick={(e) => handleMenuClick(e, setAnchorElClerk)}>
+            Clerk Pages
+          </Button>
+          <Menu
+            anchorEl={anchorElClerk}
+            open={Boolean(anchorElClerk)}
+            onClose={() => handleMenuClose(setAnchorElClerk)}
+          >
+            <MenuItem component={Link} to="/ReStock">Update Stock</MenuItem>
+            <MenuItem component={Link} to="/ClerkAddPackage">Manual Entry for New Customer Package</MenuItem>
+          </Menu>
+
+          {/* Driver Pages Dropdown */}
+          <Button color="inherit" onClick={(e) => handleMenuClick(e, setAnchorElDriver)}>
+            Driver Pages
+          </Button>
+          <Menu
+            anchorEl={anchorElDriver}
+            open={Boolean(anchorElDriver)}
+            onClose={() => handleMenuClose(setAnchorElDriver)}
+          >
+            <MenuItem component={Link} to="/PackageStatus">Update Package Status</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
 
-
-      {/*this section adds the links to these pages*/}
+      {/* This section adds the links to these pages */}
       <Container style={{ marginTop: 20 }}>
         <Routes>
-          {/*pages anyone could see when openning the app*/}
+          {/* Pages anyone could see when opening the app */}
           <Route path="/" element={<Home />} />
           <Route path="/cust-login" element={<CustLogin />} />
           <Route path="/emp-login" element={<EmpLogin />} />
           <Route path="/CustSignup" element={<CustSignup />} />
 
-          {/*pages customers should be able to see*/}
+          {/* Pages customers should be able to see */}
           <Route path="/Store" element={<Store />} />
           <Route path="/Checkout" element={<Checkout />} />
           <Route path="/MyPackages" element={<MyPackages />} />
@@ -83,30 +139,29 @@ const App = () => {
           <Route path="/CustAddPackage" element={<CustAddPackage />} />
           <Route path="/PackageCheckOut" element={<PackageCheckOut />} />
 
-          {/*EMPLOYEE PAGES*/}
+          {/* EMPLOYEE PAGES */}
           <Route path="/AddMyHours" element={<AddMyHours />} />
-          {/*pages Admin should be able to see*/}
+
+          {/* Pages Admin should be able to see */}
           <Route path="/AddPO" element={<AddPO />} />
           <Route path="/ViewPO" element={<ViewPO />} />
           <Route path="/AddMngr" element={<AddMngr />} />
           <Route path="/ViewStaff" element={<ViewStaff />} />
 
-          {/*pages Managers should be able to see*/}
+          {/* Pages Managers should be able to see */}
           <Route path="/AddStore" element={<AddStore />} />
           <Route path="/AddStaff" element={<AddStaff />} />
           <Route path="/MngrViewStaff" element={<MngrViewStaff />} />
           <Route path="/ViewStaffActivity" element={<ViewStaffActivity />} />
 
-          {/*pages Clerks should be able to see*/}
+          {/* Pages Clerks should be able to see */}
           <Route path="/ReStock" element={<ReStock />} />
           <Route path="/ClerkAddPackage" element={<ClerkAddPackage />} />
 
-          {/*pages Drivers should be able to see*/}
+          {/* Pages Drivers should be able to see */}
           <Route path="/PackageStatus" element={<PackageStatus />} />
-          
         </Routes>
       </Container>
-
     </Router>
   );
 };
