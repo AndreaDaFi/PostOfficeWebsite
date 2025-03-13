36ae46f50404cfaeaf7e503c1bd6fb0ccd0b6929
AndreaDaFi <adavila9@cougarnet.uh.edu>
2025-03-13 17:17:33 -0500

    home page is fixed

--- src/app/Home.js
+++ src/app/Home.js
@@ -3,67 +3,11 @@ import { Container, Typography, Box, Grid } from '@mui/material';
 import Cougar from '../components/Cougar.png';
 
 const Home = () => {
-  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // Fetch data from the API when the component mounts
-    fetch('https://vercel-api-powebapp.vercel.app/api/testDB')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Fetched Data:', data);
-        setData(data); // Set the fetched data
-        setLoading(false); // Set loading to false once data is fetched
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setError(error.message);
-        setLoading(false); // Set loading to false in case of error
-      });
-  }, []);
-
   return (
     <>
-      <Container>
-        <h1>City Information</h1>
-        <Box my={4}>
-          {/* Display API data or loading/error messages */}
-          {loading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>Error: {error}</p>
-          ) : data && data.success ? (
-            <Grid container spacing={2}>
-              {data.data && data.data.map((city, index) => (
-                <Grid item xs={12} sm={6} md={4} key={city.city_id}>
-                  <Box
-                    sx={{
-                      border: '1px solid #ccc',
-                      padding: 2,
-                      borderRadius: 2,
-                      backgroundColor: '#f4f4f9',
-                      boxShadow: 2,
-                      textAlign: 'center',
-                    }}
-                  >
-                    <Typography variant="h6">{`City: ${city.city_name}`}</Typography>
-                    <Typography variant="body2" color="textSecondary">{`City ID: ${city.city_id}`}</Typography>
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
-          ) : (
-            <p>No data available.</p>
-          )}
-        </Box>
-      </Container>
-
       <Container>
         {/* Header Section */}
         <Box my={4}>
